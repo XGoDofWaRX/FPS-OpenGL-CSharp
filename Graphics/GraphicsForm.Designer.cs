355011881f59d8837ad85895ef672341efa62cdc
Hossam Ali <hossameldinali96@gmail.com>
2019-04-22 02:55:39 +0200

    Adding Project Files
    Adding Current Project Files.
    The Current Progress:
    •FPS Camera with movement
    •Skybox with its light
    •some models

---
+++ Graphics/GraphicsForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Graphics
+{
+    partial class GraphicsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(765, 439);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.simpleOpenGlControl1_KeyPress);
+            this.simpleOpenGlControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.simpleOpenGlControl1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(821, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 39);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Send Data to GPU";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(793, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Specular Exponent:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(931, 211);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(53, 22);
+            this.textBox5.TabIndex = 20;
+            this.textBox5.Text = "50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(794, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Attenuation:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(884, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(794, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Blue:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(849, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 22);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.Text = "0.06";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(794, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Green:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(849, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "0.03";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(794, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Red:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(849, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "0.1";
+            // 
+            // GraphicsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 439);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "GraphicsForm";
+            this.Text = "Graphics";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GraphicsForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Graphics/Renderer.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Tao.OpenGl;
+using GlmNet;
+using System.IO;
+using Graphics._3D_Models;
+namespace Graphics
+{
+    class Renderer
+    {
+        Shader sh;
+        int transID;
+        int viewID;
+        int projID;
+
+        int EyePositionID;
+        int AmbientLightID;
+        int DataID;
+
+        mat4 ProjectionMatrix;
+        mat4 ViewMatrix;
+
+        public float Speed = 1;
+        public float skyboxSize = 100.0f;
+
+        uint vertexBufferID;
+
+        public Camera cam;
+
+        public md2LOL zombie;
+        public md2LOL zombie2;
+        public Model3D jeep;
+        public Model3D jeep2;
+        public Model3D house;
+
+        Texture up;
+        Texture down;
+        Texture left;
+        Texture right;
+        Texture front;
+        Texture back;
+
+        mat4 modelmatrix;
+        public void Initialize()
+        {
+            string projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            sh = new Shader(projectPath + "\\Shaders\\SimpleVertexShader.vertexshader", projectPath + "\\Shaders\\SimpleFragmentShader.fragmentshader");
+
+            up = new Texture(projectPath + "\\Textures\\SunSetUp2048.png", 2);
+            down = new Texture(projectPath + "\\Textures\\grass.png", 3, true);
+            right = new Texture(projectPath + "\\Textures\\SunSetLeft2048.png", 4, true);
+            left = new Texture(projectPath + "\\Textures\\SunSetRight2048.png", 5, true);
+            front = new Texture(projectPath + "\\Textures\\SunSetFront2048.png", 6, true);
+            back = new Texture(projectPath + "\\Textures\\SunSetBack2048.png", 7, true);
+
+            //zombie
+            zombie = new md2LOL(projectPath + "\\ModelFiles\\zombie.md2");
+            zombie.StartAnimation(animType_LOL.ATTACK1);
+            zombie.rotationMatrix = glm.rotate((float)((-90.0f / 180) * Math.PI), new vec3(1, 0, 0));
+            zombie.scaleMatrix = glm.scale(new mat4(1), new vec3(0.1f, 0.1f, 0.1f));
+            zombie.TranslationMatrix = glm.translate(new mat4(1), new vec3(10, 0, 0));
+            //zombie2
+            zombie2 = new md2LOL(projectPath + "\\ModelFiles\\zombie.md2");
+            zombie2.StartAnimation(animType_LOL.ATTACK2);
+            zombie2.rotationMatrix = glm.rotate((float)((-90.0f / 180) * Math.PI), new vec3(1, 0, 0));
+            zombie2.scaleMatrix = glm.scale(new mat4(1), new vec3(0.1f, 0.1f, 0.1f));
+            zombie2.TranslationMatrix = glm.translate(new mat4(1), new vec3(-10, 0, 0));            
+
+            //House
+            house = new Model3D();
+            house.LoadFile(projectPath + "\\ModelFiles\\models\\3DS\\House", 9, "house.obj");                       
+
+            //jeep
+            jeep = new Model3D();
+            jeep.LoadFile(projectPath + "\\ModelFiles\\models\\3DS\\jeep", 10, "jeep1.3ds");
+            jeep.scalematrix = glm.scale(new mat4(1), new vec3(0.3f, 0.3f, 0.3f));
+            jeep.transmatrix = glm.translate(new mat4(1), new vec3(-6, 0, 0));
+            jeep.rotmatrix = glm.rotate((float)((-90.0f / 180) * Math.PI), new vec3(1, 0, 0));
+
+            //jeep2
+            jeep2 = new Model3D();
+            jeep2.LoadFile(projectPath + "\\ModelFiles\\models\\3DS\\jeep", 10, "jeep1.3ds");
+            jeep2.scalematrix = glm.scale(new mat4(1), new vec3(0.3f, 0.3f, 0.3f));
+            jeep2.transmatrix = glm.translate(new mat4(1), new vec3(6, 0, 0));
+            jeep2.rotmatrix = glm.rotate((float)((-90.0f / 180) * Math.PI), new vec3(1, 0, 0));
+
+            float[] skybox = {
+                //up
+                -skyboxSize, skyboxSize, skyboxSize,        //0
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, -skyboxSize,       //1
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 -skyboxSize, skyboxSize, -skyboxSize,      //2
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, skyboxSize,        //3
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                -skyboxSize, skyboxSize, skyboxSize,        //4
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, -skyboxSize,       //5
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+                 
+                //ground
+                -skyboxSize, 0.0f, skyboxSize,        //6
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, -skyboxSize,       //7
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 -skyboxSize, 0.0f, -skyboxSize,      //8
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, skyboxSize,        //9
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                -skyboxSize, 0.0f, skyboxSize,        //10
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, -skyboxSize,       //11
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 //left
+                -skyboxSize, 0.0f, skyboxSize,        //12
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 -skyboxSize, skyboxSize, -skyboxSize,       //13
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 -skyboxSize, skyboxSize, skyboxSize,      //14
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 -skyboxSize, 0.0f, -skyboxSize,        //15
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                 -skyboxSize, 0.0f, skyboxSize,        //16
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 -skyboxSize, skyboxSize, -skyboxSize,       //17
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 //right
+                 skyboxSize, 0.0f, skyboxSize,        //18
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, -skyboxSize,       //19
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, -skyboxSize,      //20
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, skyboxSize,        //21
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, skyboxSize,        //22
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, -skyboxSize,       //23
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 //front
+                 -skyboxSize, 0.0f, -skyboxSize,        //24
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, -skyboxSize,       //25
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 -skyboxSize, skyboxSize, -skyboxSize,      //26
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, -skyboxSize,        //27
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                 -skyboxSize, 0.0f, -skyboxSize,        //28
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, -skyboxSize,       //29
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 //back
+                 -skyboxSize, 0.0f, skyboxSize,        //30
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, skyboxSize,       //31
+                 0,0,1,
+                 0,1,
+                 0,1,0,
+
+                 -skyboxSize, skyboxSize, skyboxSize,      //32
+                 0,0,1,
+                 1,1,
+                 0,1,0,
+
+                 skyboxSize, 0.0f, skyboxSize,        //33
+                 0,0,1,
+                 0,0,
+                 0,1,0,
+
+                 -skyboxSize, 0.0f, skyboxSize,        //34
+                 0,0,1,
+                 1,0,
+                 0,1,0,
+
+                 skyboxSize, skyboxSize, skyboxSize,       //35
+                 0,0,1,
+                 0,1,
+                 0,1,0
+            };
+            vertexBufferID = GPU.GenerateBuffer(skybox);
+
+            Gl.glClearColor(0, 0, 0.4f, 1);
+            cam = new Camera();
+            cam.Reset(0, 4, 20, 0, 0, 0, 0, 1, 0);
+
+            ProjectionMatrix = cam.GetProjectionMatrix();
+            ViewMatrix = cam.GetViewMatrix();
+
+            transID = Gl.glGetUniformLocation(sh.ID, "trans");
+            projID = Gl.glGetUniformLocation(sh.ID, "projection");
+            viewID = Gl.glGetUniformLocation(sh.ID, "view");
+
+
+            modelmatrix = glm.scale(new mat4(1), new vec3(1, 1, 1));
+
+            sh.UseShader();
+
+            DataID = Gl.glGetUniformLocation(sh.ID, "data");
+            vec2 data = new vec2(5, 50);
+            Gl.glUniform2fv(DataID, 1, data.to_array());
+
+            int LightPositionID = Gl.glGetUniformLocation(sh.ID, "LightPosition_worldspace");
+            vec3 lightPosition = new vec3(100.0f, 55.0f, 48.0f);
+            Gl.glUniform3fv(LightPositionID, 1, lightPosition.to_array());
+            //setup the ambient light component.
+            AmbientLightID = Gl.glGetUniformLocation(sh.ID, "ambientLight");
+            vec3 ambientLight = new vec3(1.5f, 1.3f, 1.3f);
+            Gl.glUniform3fv(AmbientLightID, 1, ambientLight.to_array());
+            //setup the eye position.
+            EyePositionID = Gl.glGetUniformLocation(sh.ID, "EyePosition_worldspace");
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glDepthFunc(Gl.GL_LESS);
+        }
+
+        public void Draw()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT|Gl.GL_DEPTH_BUFFER_BIT);
+            sh.UseShader();
+
+            Gl.glUniformMatrix4fv(projID, 1, Gl.GL_FALSE, ProjectionMatrix.to_array());
+            Gl.glUniformMatrix4fv(viewID, 1, Gl.GL_FALSE, ViewMatrix.to_array());
+
+            Gl.glUniform3fv(EyePositionID, 1, cam.GetCameraPosition().to_array());
+
+            zombie.Draw(transID);
+            zombie2.Draw(transID);
+            house.Draw(transID);
+            jeep.Draw(transID);
+            jeep2.Draw(transID);
+
+            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, vertexBufferID);
+            Gl.glUniformMatrix4fv(transID, 1, Gl.GL_FALSE, modelmatrix.to_array());
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 11 * sizeof(float), IntPtr.Zero);
+            Gl.glEnableVertexAttribArray(1);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 11 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+            Gl.glEnableVertexAttribArray(2);
+            Gl.glVertexAttribPointer(2, 2, Gl.GL_FLOAT, Gl.GL_FALSE, 11 * sizeof(float), (IntPtr)(6 * sizeof(float)));
+            Gl.glEnableVertexAttribArray(3);
+            Gl.glVertexAttribPointer(3, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 11 * sizeof(float), (IntPtr)(8 * sizeof(float)));
+            up.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+            down.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 6, 6);
+            left.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 12, 6);
+            right.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 18, 6);
+            front.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 24, 6);
+            back.Bind();
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 30, 6);
+        }
+        public void Update(float deltaTime)
+        {
+            cam.UpdateViewMatrix();
+            ProjectionMatrix = cam.GetProjectionMatrix();
+            ViewMatrix = cam.GetViewMatrix();
+            zombie.UpdateExportedAnimation();
+            zombie2.UpdateExportedAnimation();            
+        }
+        public void SendLightData(float red, float green, float blue, float attenuation, float specularExponent)
+        {
+            vec3 ambientLight = new vec3(red, green, blue);
+            Gl.glUniform3fv(AmbientLightID, 1, ambientLight.to_array());
+            vec2 data = new vec2(attenuation, specularExponent);
+            Gl.glUniform2fv(DataID, 1, data.to_array());
+        }
+        public void CleanUp()
+        {
+            sh.DestroyShader();
+        }
+    }
+}
